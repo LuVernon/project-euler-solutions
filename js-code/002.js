cf6c0b01d9d6cfc6730a0b0e7b0631aec194cdb1
LuVernon <lucasvernon@icloud.com>
2023-10-03 16:02:58 -0700

    Cleaned up indentation

--- js-code/002.js
+++ js-code/002.js
@@ -1,18 +1,18 @@
 let fibNums = [1, 2]; 
-        let evenSum = 2; 
+let evenSum = 2; 
 
-        function listFibs (n) {
-            for (i=2; i<n; i++) {  
-                if (fibNums[i-2] + fibNums[i-1] > n) {
-                    return evenSum;
-                }
-                else if (fibNums[i-2] + fibNums[i-1] <= n) {
-                    fibNums.push(fibNums[i-2] + fibNums[i-1]);
-                    if (Number(fibNums[i]) % 2 == 0) {
-                        evenSum += Number(fibNums[i]);
-                    }
-                }
+function listFibs (n) {
+    for (i=2; i<n; i++) {  
+        if (fibNums[i-2] + fibNums[i-1] > n) {
+            return evenSum;
+        }
+        else if (fibNums[i-2] + fibNums[i-1] <= n) {
+            fibNums.push(fibNums[i-2] + fibNums[i-1]);
+            if (Number(fibNums[i]) % 2 == 0) {
+                evenSum += Number(fibNums[i]);
             }
         }
+    }
+}
 
         listFibs(34);
