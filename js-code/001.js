c63b61ddbee64a3ba3b9b5daab67a37ad8feb782
LuVernon <lucasvernon@icloud.com>
2023-10-03 15:55:34 -0700

    Problems 001 and 002 written and pushed in pure js.

---
+++ js-code/001.js
@@ -0,0 +1,12 @@
+var sum = 0; 
+
+        function multiplesOf3and5(number) {
+            for (let i=3; i<number; i++) {
+                if (i % 3 == 0 || i % 5 == 0) {
+                sum += i; 
+                }
+            }
+            return sum;
+        }
+
+        multiplesOf3and5(1000);
